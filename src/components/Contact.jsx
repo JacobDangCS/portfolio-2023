9a703f553b23e9c4069ba33fab31b53698ad5bc0
JacobDangCS50 <jacobohdang@gmail.com>
2023-08-04 00:17:09 +0000

    fix bug

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div name="contact" className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
-            <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto">
+            <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-3/5">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500"> Contact </p>
                     <p className="py-6">Contact information is available on my resume!</p>
