09f6c78176474e62442ce47d180dd7d30f2622ea
JacobDangCS50 <jacobohdang@gmail.com>
2023-08-04 00:09:19 +0000

    changed contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,28 +3,10 @@ import React from 'react';
 const Contact = () => {
     return (
         <div name="contact" className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
-            <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+            <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500"> Contact </p>
-                    <p className="py-6">Submit a form below if you're looking to reach out!</p>
-                </div>
-                <div className="flex justify-center items-center">
-                    <form action="https://getform.io/f/ef92d45e-e1d8-41ac-9646-742a607ccdc8"
-                        method="POST" className="flex flex-col w-full md:w-1/2">
-                        <input type="text" name="name" placeholder="Enter your name"
-                            className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
-
-                        <input type="text" name="email" placeholder="Enter your email"
-                            className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
-
-                        <textarea name="message" placeholder="Enter your message" rows="10"
-                            className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"></textarea>
-
-                        <button
-                            className="text-white bg-gradient-to-b from-green-500 to-green-900 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-                            Let's chat
-                        </button>
-                    </form>
+                    <p className="py-6">Contact information is available on my resume!</p>
                 </div>
             </div>
         </div>
